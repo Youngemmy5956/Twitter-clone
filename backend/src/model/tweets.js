84cdffe0e73d4d74a5a212160f415492b9f15ac6
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-06-29 18:00:32 +0100

    added tweet api with date

--- backend/src/model/tweets.js
+++ backend/src/model/tweets.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const tweetSchema = mongoose.Schema({
     tweet: { type: String, required: true },
+    date: { type: Date, default: Date.now },
 });
 
 const Tweet_model = mongoose.model("Tweets", tweetSchema);
